aef74b4ad1582543b7fb1859a895a642beedf3c5
Aozin <59066161+Aozin@users.noreply.github.com>
2022-02-24 19:40:48 +0000

    adding code bits

--- src/Combiner.java
+++ src/Combiner.java
@@ -6,14 +6,41 @@ public class Combiner {
         int days = 0;
         int score = 0;
         while(projectsComplete < projects.size()){
+            ArrayList<Project> incompleteProjs = new ArrayList<>();
             for (int i = 0; i < projects.size(); i++){
                 if(projects.get(i).getComplete()){
                     score = score + projects.get(i).getScore(days);
                     projectsComplete += 1;
+                } else if (projects.get(i).woking.size() == 0){
+                    incompleteProjs.add(projects.get(i));
                 }
             }
 
             // Code here for assigning people
+            ArrayList<Contributor> availableConts = new ArrayList<>();
+            for (Contributor i : contributors) {
+                if (!i.isWorking) {
+                    availableConts.add(i);
+                } 
+            }
+
+            //sort incompleteProjs
+
+
+            for (Project i : incompleteProjs) {
+
+                //sort contributors by points
+                
+                //grab people
+                ArrayList<Skill> requiredSkills = i.roles;
+                ArrayList<Contributor> currentCont = new ArrayList<>();
+                for (Skill j : requiredSkills) {
+
+                }
+
+            }
+
+
 
 
             for (int i = 0; i < projects.size(); i++){
