875d6a48095d1945047c0227539c33c07a91af2e
Aozin <59066161+Aozin@users.noreply.github.com>
2022-02-24 20:35:36 +0000

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,12 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
+        ArrayList<Contributor> test = new ArrayList<>();
+        Parser.getinfo("./res/a_an_example.in.txt");
 
-        
-        
+        System.out.println(Parser.conts.size());
+        Combiner lmao = new Combiner(Parser.projs, Parser.conts);
+        Output.write();
     }
 }
     
