601b3b9970cfdfdcd2dd4b3bcd05957af6d6b31c
Aozin <59066161+Aozin@users.noreply.github.com>
2022-02-24 20:49:20 +0000

    ;

--- src/Output.java
+++ src/Output.java
@@ -10,7 +10,7 @@ public class Output {
         pog.append(projectsDone.size() + "\n");
         for (Project i : projectsDone) {
             pog.append(i.name + "\n");
-            pog.append(i.woking.get(0).contrib.name)
+            pog.append(i.woking.get(0).contrib.name);
             for (WorkingOn j : i.woking) {
                 pog.append(" " + j.contrib.name);
             }
