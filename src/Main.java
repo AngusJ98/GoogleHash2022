8ae202ddf7fe3a7d277dcd87e43bac7c3acc8c19
Asmondia <maddisonben@live.com>
2022-02-24 18:44:24 +0000

    Fix File

--- src/Main.java
+++ src/Main.java
@@ -6,8 +6,8 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
         ArrayList<Contributor> test = new ArrayList<>();
-        Parser.getinfo("a_an_example.in.txt");
+        Parser.getinfo("GoogleHash2022/res/a_an_example.in.txt");
         System.out.println(Parser.conts.toString());
     }
-    
 }
+    
