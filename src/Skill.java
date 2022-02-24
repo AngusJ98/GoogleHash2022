91f04b2da9cab7a2ee0e3198d332e94b725b7150
Asmondia <maddisonben@live.com>
2022-02-24 18:06:17 +0000

    Skill Made

---
+++ src/Skill.java
@@ -0,0 +1,10 @@
+public class Skill {
+    public String skillName;
+    public int skillLevel;
+    public Skill(String skillName, int SkillLevel){
+        skillName = skillName;
+        skillLevel = SkillLevel;
+
+    }
+    
+}
