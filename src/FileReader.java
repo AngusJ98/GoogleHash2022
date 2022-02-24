a68feaadefda38740640daae9b6ba1d2919fef87
Aozin <59066161+Aozin@users.noreply.github.com>
2022-02-24 17:59:37 +0000

    copied lmao

---
+++ src/FileReader.java
@@ -0,0 +1,29 @@
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class FileReader {
+    public static ArrayList<String[]> readFileAsIntArray(String fileName) {
+        File inputFile = new File (fileName);
+        Scanner in = null;
+        try {
+            in = new Scanner (inputFile);
+        } catch (FileNotFoundException e) {
+            System.out.println ("Cannot open " + fileName);
+            System.exit (0);
+        }
+        ArrayList<String[]> result = new ArrayList<>();
+
+        while(in.hasNextLine()) {
+            String textToAdd = in.nextLine();
+            String[] strArray = textToAdd.split(" ");
+            result.add(strArray);
+        }
+
+        in.close ();
+        return result;
+
+    }
+}
