2cd858a9e9093d5f7af1a4baf62f74412124c9a1
Aozin <59066161+Aozin@users.noreply.github.com>
2022-02-24 20:51:45 +0000

    format fix

--- src/Output.java
+++ src/Output.java
@@ -10,8 +10,9 @@ public class Output {
         pog.append(projectsDone.size() + "\n");
         for (Project i : projectsDone) {
             pog.append(i.name + "\n");
-            for (WorkingOn j : i.woking) {
-                pog.append(j.contrib.name + " ");
+            pog.append(i.woking.get(0).contrib.name);
+            for (int j = 1; j < i.woking.size(); j++) {
+                pog.append(" " + i.woking.get(j).contrib.name);
             }
             pog.append("\n");
         }
