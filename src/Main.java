7012f7e167e9dd26ab2fa0497163b5388700ad79
Aozin <59066161+Aozin@users.noreply.github.com>
2022-02-24 18:23:33 +0000

    try this pls

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
+
+
+import java.util.ArrayList;
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        ArrayList<Contributor> test = new ArrayList<>();
+        Parser.getinfo("a_an_example.in.txt");
+        System.out.println(Parser.conts.toString());
     }
     
 }
