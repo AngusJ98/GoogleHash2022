9f91dc4648fdaf3faa458e4c6fbef43e9735ab7a
Asmondia <maddisonben@live.com>
2022-02-24 18:09:07 +0000

    con done

--- src/Contributor.java
+++ src/Contributor.java
@@ -1,2 +1,10 @@
+import java.util.ArrayList;
+
 public class Contributor {
+    public ArrayList<Skill> skills;
+    public String name;
+    public Contributor(String name, ArrayList<Skill> skills){
+        name = name;
+        skills = skills;
+    }
 }
--- src/Skill.java
+++ src/Skill.java
@@ -6,5 +6,5 @@ public class Skill {
         skillLevel = SkillLevel;
 
     }
-    
+
 }
